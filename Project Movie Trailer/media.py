9f8e012ffb095eafc7eec0cc208fb8ecc42a8065
Codersean <godwinojebiyi@gmail.com>
2017-06-16 10:58:11 +0100

    Made Changes as requested by the reviewer

--- Project Movie Trailer/media.py
+++ Project Movie Trailer/media.py
@@ -1,6 +1,6 @@
 # Class Movie
 class Movie():
-    #Initialization of the movie Class
+    # Initialization of the movie Class
     def __init__(self, title, image_url, youtube_url):
         self.title = title
         self.poster_image_url = image_url
