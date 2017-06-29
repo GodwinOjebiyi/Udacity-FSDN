d7faf27caf47f633de5b635d150a78b6d9c92c82
Codersean <godwinojebiyi@gmail.com>
2017-06-29 17:07:49 +0100

    Removed Unused Files and example files

--- Project Portfolio Site/js/modal.js
+++
@@ -1,6 +0,0 @@
-//Javascript Source: http://jsfiddle.net/jschr/3kgbG/
-
-$('.launchConfirm').on('click', function (e) {
-    $('#confirm')
-        .modal({ backdrop: 'static', keyboard: false })     
-});
